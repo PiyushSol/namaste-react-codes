bbf2232ec78ca3cb56a7886e566037ba5ee5379d
Piyush <solankipiyush942@gmail.com>
2025-06-05 23:30:24 +0530

    05-06-2025

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
 import React from "react";
 import { createRoot } from "react-dom/client"
 
+/*
+            Header
+             -Logo
+             -NavItems(Right Side)
+             -Cart
+            Body
+             - Search Bar
+             - Restraunt List
+              - Restraunt Card
+               -Image
+               -Description 
+               -Name
+               -Star rating
+            Footer
+            -Links
+            -Copyrights   
+            */
+
 // const heading1 = React.createElement(
 //     "h1",
 //     {
@@ -14,25 +32,779 @@ import { createRoot } from "react-dom/client"
 //When you span more than one line enlcose them in paranthesis
 
 //This is a react element.
- const heading = (<h1 id="title" key="h1">
- Namaste React
- </h1>
+ const Title =() => (
+<a href="/"><img className="title-logo" src="https://a.mktgcdn.com/p/HVoJy7Sqyw_sopyJMvKklguS-vlAj9LiXbF3glb7xeY/400x400.jpg" alt="App logo" />
+</a>
  );
 
  //Component
 //Functional component
-const HeaderComponent = ()=> (
-    <div>
-    {heading}
-    <h1>
-        Namaste React Functional Component
-    </h1>
-    <h2>
-    This is H2 component.
-    </h2>
+const Header = ()=> (
+    <div className="header">
+    <Title/>
+    <div className="nav-items">
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Cart</li>
+        </ul>
+    </div>
      </div>
      );
 
+const restrauntList =[
+   {
+"info": {
+"id": "1003414",
+"name": "Pizza Hut",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/5/22/6c8a7784-0b42-419e-81f9-f2ab2319a479_1003414.JPG",
+"locality": "Chhindwara",
+"areaName": "Parasia Road",
+"costForTwo": "₹350 for two",
+"cuisines": [
+"Pizzas"
+],
+"avgRating": 4.2,
+"parentId": "721",
+"avgRatingString": "4.2",
+"totalRatingsString": "104",
+"sla": {
+"deliveryTime": 46,
+"lastMileTravel": 13.1,
+"serviceability": "SERVICEABLE",
+"slaString": "45-50 mins",
+"lastMileTravelString": "13.1 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-06-06 00:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹59"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"isNewlyOnboarded": true,
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-09ba05ec-9ffe-490c-a9b4-8afca2305472"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/pizza-hut-parasia-road-rest1003414",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "234875",
+"name": "Adil Hotel",
+"cloudinaryImageId": "gp1ityra6utvzqn6ghnv",
+"locality": "Rautha Wada",
+"areaName": "Chhindwara Locality",
+"costForTwo": "₹150 for two",
+"cuisines": [
+"North Indian",
+"Biryani",
+"Tandoor"
+],
+"avgRating": 4.3,
+"parentId": "27123",
+"avgRatingString": "4.3",
+"totalRatingsString": "1.3K+",
+"sla": {
+"deliveryTime": 38,
+"lastMileTravel": 11.5,
+"serviceability": "SERVICEABLE",
+"slaString": "35-40 mins",
+"lastMileTravelString": "11.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-06-06 00:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹129"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-09ba05ec-9ffe-490c-a9b4-8afca2305472"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/adil-hotel-rautha-wada-chhindwara-locality-rest234875",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "151656",
+"name": "Dev International",
+"cloudinaryImageId": "enj3srsnhbltbom2ovvh",
+"locality": "khajri road",
+"areaName": "Mohan Nagar",
+"costForTwo": "₹100 for two",
+"cuisines": [
+"North Indian",
+"Chinese",
+"Fast Food",
+"Beverages"
+],
+"avgRating": 4.3,
+"parentId": "71556",
+"avgRatingString": "4.3",
+"totalRatingsString": "510",
+"sla": {
+"deliveryTime": 50,
+"lastMileTravel": 13.6,
+"serviceability": "SERVICEABLE",
+"slaString": "50-55 mins",
+"lastMileTravelString": "13.6 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-06-05 22:30:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "₹100 OFF",
+"subHeader": "ABOVE ₹299",
+"discountTag": "FLAT DEAL"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-09ba05ec-9ffe-490c-a9b4-8afca2305472"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/dev-international-khajri-road-mohan-nagar-rest151656",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "618037",
+"name": "Jai Ganesh Bhojnalaya",
+"cloudinaryImageId": "yzgqriufpzmloogcn2vl",
+"locality": "Railway Colony",
+"areaName": "Bus stand",
+"costForTwo": "₹200 for two",
+"cuisines": [
+"North Indian",
+"South Indian",
+"Indian",
+"Chinese"
+],
+"avgRating": 4.1,
+"veg": true,
+"parentId": "368432",
+"avgRatingString": "4.1",
+"totalRatingsString": "393",
+"sla": {
+"deliveryTime": 43,
+"lastMileTravel": 13.7,
+"serviceability": "SERVICEABLE",
+"slaString": "40-45 mins",
+"lastMileTravelString": "13.7 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-06-05 23:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"description": "pureveg"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "pureveg",
+"imageId": "v1695133679/badges/Pure_Veg111.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "₹66 OFF",
+"subHeader": "ABOVE ₹249",
+"discountTag": "FLAT DEAL"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-09ba05ec-9ffe-490c-a9b4-8afca2305472"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/jai-ganesh-bhojnalaya-railway-colony-bus-stand-rest618037",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "151518",
+"name": "Bakery World",
+"cloudinaryImageId": "mt2aggiscfl3yviatwng",
+"locality": "Parasia Road",
+"areaName": "Parasia Road",
+"costForTwo": "₹250 for two",
+"cuisines": [
+"Bakery",
+"Ice Cream",
+"Snacks",
+"Beverages"
+],
+"avgRating": 4.4,
+"veg": true,
+"parentId": "40363",
+"avgRatingString": "4.4",
+"totalRatingsString": "268",
+"sla": {
+"deliveryTime": 42,
+"lastMileTravel": 12.4,
+"serviceability": "SERVICEABLE",
+"slaString": "40-45 mins",
+"lastMileTravelString": "12.4 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-06-05 22:30:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"description": "pureveg"
+}
+]
+},
+"isOpen": true,
+"aggregatedDiscountInfoV2": {},
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "pureveg",
+"imageId": "v1695133679/badges/Pure_Veg111.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-09ba05ec-9ffe-490c-a9b4-8afca2305472"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/bakery-world-parasia-road-rest151518",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "630193",
+"name": "Champaran  Handi Restaurant",
+"cloudinaryImageId": "xph0vf5pvbxjf8xchwjk",
+"locality": "Sinchai Colony",
+"areaName": "Nagpur road",
+"costForTwo": "₹200 for two",
+"cuisines": [
+"North Indian"
+],
+"avgRating": 4.3,
+"parentId": "376324",
+"avgRatingString": "4.3",
+"totalRatingsString": "412",
+"sla": {
+"deliveryTime": 41,
+"lastMileTravel": 11.9,
+"serviceability": "SERVICEABLE",
+"slaString": "40-45 mins",
+"lastMileTravelString": "11.9 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-06-05 23:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"aggregatedDiscountInfoV2": {},
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-09ba05ec-9ffe-490c-a9b4-8afca2305472"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/champaran-handi-restaurant-sinchai-colony-nagpur-road-rest630193",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "615927",
+"name": "Roof 5",
+"cloudinaryImageId": "glepanv4xjfnhsbieam1",
+"locality": "Adarsh Nagar Colony",
+"areaName": "Chhindwara Locality",
+"costForTwo": "₹200 for two",
+"cuisines": [
+"Indian"
+],
+"avgRating": 4.2,
+"parentId": "171348",
+"avgRatingString": "4.2",
+"totalRatingsString": "173",
+"sla": {
+"deliveryTime": 53,
+"lastMileTravel": 14.5,
+"serviceability": "SERVICEABLE",
+"slaString": "50-55 mins",
+"lastMileTravelString": "14.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-06-05 23:45:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "30% OFF",
+"subHeader": "UPTO ₹150",
+"discountTag": "SAVE BIG"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-09ba05ec-9ffe-490c-a9b4-8afca2305472"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/roof-5-adarsh-nagar-colony-chhindwara-locality-rest615927",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "151648",
+"name": "Mr. Gurung Momo & Chinese Corner",
+"cloudinaryImageId": "h7oibtpm7kqks8cqz3gh",
+"locality": "Teacher's Colony",
+"areaName": "Mohan Nagar",
+"costForTwo": "₹150 for two",
+"cuisines": [
+"Momos",
+"Chinese",
+"Fast Food"
+],
+"avgRating": 4.3,
+"veg": true,
+"parentId": "140255",
+"avgRatingString": "4.3",
+"totalRatingsString": "1.1K+",
+"sla": {
+"deliveryTime": 38,
+"lastMileTravel": 13.2,
+"serviceability": "SERVICEABLE",
+"slaString": "35-40 mins",
+"lastMileTravelString": "13.2 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-06-05 23:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"description": "pureveg"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "pureveg",
+"imageId": "v1695133679/badges/Pure_Veg111.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹49"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-09ba05ec-9ffe-490c-a9b4-8afca2305472"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/mr-gurung-momo-and-chinese-corner-teachers-colony-mohan-nagar-rest151648",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+}
+]
+
+// const pizzaHut = {
+//     name:"Pizza Hut",
+//     image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/5/22/6c8a7784-0b42-419e-81f9-f2ab2319a479_1003414.JPG",
+//     cuisines:["Overloaded Pizza ","Cheese Burst","Margarita Pizza"],
+//     rating:"4.2"
+// }    
+
+
+//we can also use destructuing like {restraunt} then we would not need props to get object
+// const RestrauntCard = (props)=>{
+//     // console.log(props)
+//     return (
+//         <div className="card">
+//             <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+props.restraunt?.info?.cloudinaryImageId}/>
+//             <h2>{props.restraunt?.info?.name}</h2>
+//             <h3>{props.restraunt?.info?.cuisines.join(", ")}</h3>
+//             <h4>{props.restraunt?.info?.avgRatingString} Stars</h4>
+//         </div>
+//     );
+// }   
+
+const RestrauntCard = ({restraunt})=>{
+    // console.log(props)
+    const {cloudinaryImageId,name,cuisines,avgRatingString} = restraunt.info;
+    //this is destructuring the object
+    return (
+        <div className="card">
+            <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}/>
+            <h2>{name}</h2>
+            <h3>{cuisines}</h3>
+            <h4>{avgRatingString} Stars</h4>
+        </div>
+    );
+} 
+
+//config driven ui
+// const config =[
+//     {
+//         type:"carousel",
+//         cards:[
+//             {
+//                 offerName :"50% off"
+//             }
+//         ]
+//     }
+// ]
+
+const Body = ()=>{
+    return (
+     <div className="restraunt-list">
+     <RestrauntCard restraunt = {restrauntList[0]} />
+     <RestrauntCard restraunt = {restrauntList[1]} />
+     <RestrauntCard restraunt = {restrauntList[2]} />
+     <RestrauntCard restraunt = {restrauntList[3]} />
+     <RestrauntCard restraunt = {restrauntList[4]} />
+     <RestrauntCard restraunt = {restrauntList[5]} />
+     <RestrauntCard restraunt = {restrauntList[6]} />
+     </div>
+    );
+}     
+
+
+const Footer = ()=>{
+    return (
+           <h4>
+            Body
+        </h4>
+    );
+}
+
+const AppLayout=()=>{
+   return (
+   <>  
+   <Header/>
+    <Body/>
+    <Footer/>
+   </>
+   );
+};
+
  
 const root = createRoot(document.getElementById("root"));
-root.render(<HeaderComponent/>);
+root.render(<AppLayout/>);
